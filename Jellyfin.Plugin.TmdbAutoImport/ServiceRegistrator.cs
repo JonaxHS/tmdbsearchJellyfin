9cfc0841d4da76e4bd8d5232f2f1749a08edd2fa
Jonathan <jonathan@example.com>
2026-04-24 10:40:59 -0600

    Fix plugin startup malfunction

--- Jellyfin.Plugin.TmdbAutoImport/ServiceRegistrator.cs
+++ Jellyfin.Plugin.TmdbAutoImport/ServiceRegistrator.cs
@@ -1,5 +1,6 @@
 using System;
 using MediaBrowser.Controller;
+using Jellyfin.Plugin.TmdbAutoImport.Configuration;
 using Jellyfin.Plugin.TmdbAutoImport.Services;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,10 +11,7 @@ public sealed class ServiceRegistrator : IPluginServiceRegistrator
 {
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost _)
     {
-        var config = Plugin.Instance?.Configuration
-            ?? throw new InvalidOperationException("Plugin instance is not initialized.");
-
-        serviceCollection.AddSingleton(config);
+        serviceCollection.AddSingleton(_ => Plugin.Instance?.Configuration ?? new PluginConfiguration());
         serviceCollection.AddSingleton<ImportService>();
         serviceCollection.AddHttpClient<TmdbClient>();
     }
